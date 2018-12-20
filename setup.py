ae80cf54db73c210a0c3b8f63dc0715840dad5a3
williamfzc <178894043@qq.com>
2018-12-20 20:37:07 +0800

    release: 0.1.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyminitouch',
-    version='0.0.1',
+    version='0.1.0',
     description='python wrapper of minitouch, for better experience',
     author='williamfzc',
     author_email='fengzc@vip.qq.com',
