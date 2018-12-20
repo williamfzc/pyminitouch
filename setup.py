7553ed1c1ad2efc563afbd1cb07346c3452539e4
williamfzc <178894043@qq.com>
2018-12-20 17:06:45 +0800

    release: 0.0.1

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='pyminitouch',
+    version='0.0.1',
+    description='python wrapper of minitouch, for better experience',
+    author='williamfzc',
+    author_email='fengzc@vip.qq.com',
+    url='https://github.com/williamfzc/pyminitouch',
+    packages=find_packages(),
+    install_requires=[
+        'loguru',
+    ]
+)
