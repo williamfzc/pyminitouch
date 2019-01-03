c9af2122038375d8bab24fd956fe5997baaf8eee
williamfzc <178894043@qq.com>
2019-01-03 14:50:05 +0800

    fix: maximum X and Y didn't match display size

--- demo.py
+++ demo.py
@@ -2,9 +2,13 @@ import time
 
 from pyminitouch import safe_connection, safe_device, MNTDevice
 
-
-_DEVICE_ID = '4df189487c7b6fef'
+_DEVICE_ID = '3d33076e'
 device = MNTDevice(_DEVICE_ID)
+
+# It's also very important to note that the maximum X and Y coordinates may, but usually do not, match the display size.
+# so you need to calculate position by yourself, and you can get maximum X and Y by this way:
+print('max x: ', device.connection.max_x)
+print('max y: ', device.connection.max_y)
 
 # single-tap
 device.tap([(400, 600)])
@@ -20,7 +24,6 @@ device.tap([(400, 600)], pressure=50)
 device.tap([(400, 600)], duration=1000)
 time.sleep(1)
 
-
 # swipe
 device.swipe([(100, 100), (500, 500)])
 # of course, with duration and pressure
@@ -29,7 +32,6 @@ device.swipe([(100, 100), (400, 400), (200, 400)], duration=500, pressure=59)
 # stop minitouch
 # when it was stopped, minitouch can do nothing for device, including release.
 device.stop()
-
 
 # In another way, you needn't consider about device's life-cycle.
 # context manager will handle it
@@ -40,7 +42,6 @@ with safe_device(_DEVICE_ID) as device:
     device.tap([(400, 400), (600, 600)])
     # set the pressure, default == 100
     device.tap([(400, 600)], pressure=50)
-
 
 # What's more, you can also access low level API for further usage.
 # send raw text to it
